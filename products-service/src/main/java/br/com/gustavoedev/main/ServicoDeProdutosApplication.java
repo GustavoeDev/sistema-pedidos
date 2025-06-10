b5509fe25bd7a8c152f34898fc0b5ed5239de844
GustavoeDev <gustavoe.dev@gmail.com>
2025-06-10 19:05:44 -0300

    initial commit

---
+++ products-service/src/main/java/br/com/gustavoedev/main/ServicoDeProdutosApplication.java
@@ -0,0 +1,13 @@
+package br.com.gustavoedev.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicoDeProdutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicoDeProdutosApplication.class, args);
+	}
+
+}
---
+++ products-service/src/test/java/br/com/gustavoedev/main/ServicoDeProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gustavoedev.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicoDeProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
