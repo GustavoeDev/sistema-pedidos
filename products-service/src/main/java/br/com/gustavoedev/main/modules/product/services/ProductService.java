e70192d8820c30e28187413cc030d686534dd092
GustavoeDev <gustavoe.dev@gmail.com>
2025-06-10 22:19:18 -0300

    feat: create function to check if a product exists

--- products-service/src/main/java/br/com/gustavoedev/main/modules/product/services/ProductService.java
+++ products-service/src/main/java/br/com/gustavoedev/main/modules/product/services/ProductService.java
@@ -4,9 +4,13 @@ import br.com.gustavoedev.main.exceptions.ProductFoundException;
 import br.com.gustavoedev.main.modules.product.ProductRepository;
 import br.com.gustavoedev.main.modules.product.entities.ProductEntity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.UUID;
 
 @Service
 public class ProductService {
@@ -25,6 +29,11 @@ public class ProductService {
 
     public List<ProductEntity> getAllProducts() {
         return this.productRepository.findAll();
+    }
+
+    public ProductEntity findById(UUID id) {
+        return productRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("Produto não encontrado"));
     }
 
 }
