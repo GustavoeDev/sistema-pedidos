c2722fe41ac2a10cfc78c0c176f01a8a65328153
Hian Silva <hiansilacts@gmail.com>
2025-06-10 22:43:49 -0300

    feat: implements customers create

--- costumers-service/costumers/api/v1/views.py
+++ costumers-service/costumers/api/v1/views.py
@@ -15,3 +15,13 @@ class CustomersAPIView(APIView):
             return Response(serializer.data, status=status.HTTP_200_OK)
         else:
             return Response(data='None costumers found', status=status.HTTP_404_NOT_FOUND)
+
+    def post(self, request):
+        serializer = CustomerSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
